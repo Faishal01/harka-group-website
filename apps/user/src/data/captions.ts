c85ab86355805f4d804c6478e48dd21cd412461d
Muhammad Rafky <muhammad.rafky@amartha.com>
2026-08-23 14:07:44 +0700

    extract labels

--- apps/user/src/data/captions.ts
+++ apps/user/src/data/captions.ts
@@ -11,3 +11,44 @@ export const homePage = {
 		"Our team is here to help you find the perfect car that suits your needs and budget. Whether you're looking for a sleek sports car, a reliable family vehicle, or an eco-friendly hybrid, we have something for everyone.",
 	contactUsLabel: "Contact us",
 };
+
+export const carsPage = {
+	noResultFound: "No results found",
+};
+
+export const filterLabels = {
+	filter: "Filter",
+	make: "Make",
+	model: "Model",
+	price: "Price",
+	lowerThan: "Lower than",
+	higherThan: "Higher than",
+	color: "Color",
+	bodyType: "Body Type",
+	fuelType: "Fuel Type",
+	transmission: "Transmission",
+	condition: "Condition",
+	search: "Search",
+	all: "All",
+};
+
+export const sortLabels = {
+	sortBy: "Sort by",
+	mileageDesc: "Mileage: High to Low",
+	mileageAsc: "Mileage: Low to High",
+	priceDesc: "Price: High to Low",
+	priceAsc: "Price: Low to High",
+	yearDesc: "Year: High to Low",
+	yearAsc: "Year: Low to High",
+};
+
+export const searchLabels = {
+	make: "Make",
+	chooseMake: "Choose make",
+	model: "Model",
+	chooseModel: "Choose model",
+	condition: "Condition",
+	chooseCondition: "Choose condition",
+	search: "Search",
+	searchPlaceholder: "Search...",
+};
